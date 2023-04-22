e055de2ea7c3c60f86aa356e2a770d9d9f3bd13c
socpa linda <linda.socpa@institutsaintjean.org>
2023-04-22 12:14:12 +0100

    push des vues de login et d'enregistrement d'un user avec les différentes polices et les coleurs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'tabs',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
   },
   {
     path: 'login',
