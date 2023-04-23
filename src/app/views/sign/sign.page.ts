d161444e0ef41cdc6d95e4b732523718fc3e4917
socpa linda <linda.socpa@institutsaintjean.org>
2023-04-23 13:38:46 +0100

    delete of tabs1,tabs2,tabs3,expolre-container, final styling of login and sign-in,addition of img folder

--- src/app/views/sign/sign.page.ts
+++ src/app/views/sign/sign.page.ts
@@ -17,4 +17,19 @@ export class SignPage implements OnInit {
   ngOnInit() {
   }
 
+  public toastButtons = [
+   
+    {
+      text: 'OK',
+      role: 'cancel',
+      handler: () => { this.handlerMessage = 'Dismiss clicked'; }
+    }
+  ];
+  handlerMessage = '';
+  roleMessage = '';
+
+  setRoleMessage(event: any) {
+    const { role } = event.detail
+    this.roleMessage = `Dismissed with role: ${role}`;
+  }
 }
