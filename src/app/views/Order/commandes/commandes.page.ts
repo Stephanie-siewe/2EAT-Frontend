6705aedf2b531220aeb4b4c1421f76cb0d474934
socpa linda <linda.socpa@institutsaintjean.org>
2023-05-03 13:26:04 +0100

    mise àà jour de la page commandes

--- src/app/views/Order/commandes/commandes.page.ts
+++ src/app/views/Order/commandes/commandes.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-commandes',
@@ -12,9 +13,12 @@ import { IonicModule } from '@ionic/angular';
 })
 export class CommandesPage implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit() {
   }
 
+  gotodetails(){
+    this.route.navigate(['/grilling-details']);
+  }
 }
